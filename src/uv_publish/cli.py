206f0194b3b2a7c3594d05d0a0556a562b6ad830
Derek Wan <d.wan@icloud.com>
2025-12-17 16:51:03 +0900

    2025-12-17 16:51:03 (Wed)  > DW-Mac  > derekwan

--- src/uv_publish/cli.py
+++ src/uv_publish/cli.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 from click import command
+from rich.pretty import pretty_repr
 from typed_settings import click_options
 from utilities.click import CONTEXT_SETTINGS_HELP_OPTION_NAMES
 from utilities.logging import basic_config
 
+from uv_publish import __version__
 from uv_publish.lib import uv_publish
 from uv_publish.logging import LOGGER
 from uv_publish.settings import Settings
@@ -16,9 +18,12 @@ def _main(settings: Settings, /) -> None:
     if settings.dry_run:
         LOGGER.info("Dry run; exiting...")
         return
-    uv_publish()
+    basic_config(obj=LOGGER)
+    LOGGER.info(
+        "Running version %s with settings:\n%s...", __version__, pretty_repr(settings)
+    )
+    uv_publish(trusted_publishing=settings.trusted_publishing)
 
 
 if __name__ == "__main__":
-    basic_config(obj=LOGGER)
     _main()
